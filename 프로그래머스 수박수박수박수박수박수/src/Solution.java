b0ab638133ca85c46bb1f733743858a062233fd1
White <White@DESKTOP-ONL0M0Q>
2020-10-24 05:08:22 +0900

    2020-10-24

---
+++ 프로그래머스 수박수박수박수박수박수/src/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+	public String solution(int n) {
+		String answer = "";
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 != 0) {
+				answer += "¼ö";
+			} else {
+				answer += "¹Ú";
+			}
+		}
+		return answer;
+	}
+}
